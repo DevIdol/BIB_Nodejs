70827f1066ebda0cbba9dfb11978fb8219740cc2
DevIdol <devidol.mm@gmail.com>
2022-12-07 12:26:45 +0630

    Add Module on 7 Dec 2022

---
+++ Day01/sec03/app.js
@@ -0,0 +1,7 @@
+import { user } from "./user.js";
+
+let { username, age, position } = user;
+
+console.log(
+  `My Name is ${username}. I'm ${age} years old. My position is ${position}.`
+);
---
+++ Day01/sec03/user.js
@@ -0,0 +1,5 @@
+export let user = {
+  username: "Joe",
+  age: "23",
+  position:"FullStack Developer",
+}
