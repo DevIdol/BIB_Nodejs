f75f74011315e75522ae91a2987b1d20b4ee9189
DevIdol <devidol.mm@gmail.com>
2022-12-08 12:52:50 +0630

    Add Array.find

---
+++ Day02/Sec10/arrayFind.js
@@ -0,0 +1,8 @@
+const users = [
+  {name: "Joe", age: 23},
+  {name: "Mic", age: 28},
+  {name: "John", age: 18},
+  {name: "Theresa", age: 32},
+]
+
+console.log(users.find(user => user.age < 25)) //Array.Find can output the first element
