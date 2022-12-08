19e386d538c71b95aae750f009ad6ac92b9890b9
DevIdol <devidol.mm@gmail.com>
2022-12-08 11:39:27 +0630

    Modified Promise All

--- Day02/Sec08/promiseAll.js
+++ Day02/Sec08/promiseAll.js
@@ -1,17 +1,20 @@
 const num1 = new Promise((resolve, reject) => {
-  console.log("The first Promise is resolved.")
-  setTimeout(() => resolve(10), 1000);
+  setTimeout(() => {
+    console.log("The first Promise is resolved.");
+    resolve(10);
+  }, 1000);
 });
 
-
 const num2 = new Promise((resolve, reject) => {
-  console.log("The second Promise is resolved.")
-  setTimeout(() => resolve(20), 1000)
-})
+  setTimeout(() => {
+    console.log("The second Promise is resolved.");
+    resolve(20);
+  }, 1000);
+});
 
-Promise.all([num1, num2]).then(results => {
-  const sum = results.reduce((a,b) => a + b)
+Promise.all([num1, num2]).then((results) => {
+  const sum = results.reduce((a, b) => a + b);
 
-  console.log(`The Result is ${results}`)
-  console.log(`The total sum is ${sum}`)
-})
+  console.log(`The Result is ${results}`);
+  console.log(`The total sum is ${sum}`);
+});
