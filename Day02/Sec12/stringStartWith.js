eab6fc4153ea6a6d15e8949243770e069bbb1057
DevIdol <devidol.mm@gmail.com>
2022-12-08 13:17:51 +0630

    Add String StartWith

---
+++ Day02/Sec12/stringStartWith.js
@@ -0,0 +1,5 @@
+const title = 'Jack and Jill Went Up the Hill';
+
+console.log(title.startsWith('Jack'));//true
+console.log(title.startsWith('jack'));//false
+console.log(title.startsWith('Jill', 9));//true
