3deba49cf424132389332fb8fef0701c754507fd
DevIdol <devidol.mm@gmail.com>
2022-12-14 11:04:38 +0630

    Code Refectoring

--- Day07/routes/userRoutes.js
+++ Day07/routes/userRoutes.js
@@ -1,14 +1,15 @@
-const express = require('express')
-const { getAllUsers, createUser, getUser } = require("../controller/userController")
+const express = require("express");
+const {
+  getAllUsers,
+  createUser,
+  getUser,
+} = require("../controller/userController");
 const router = express.Router();
 
+router.get("/", getAllUsers);
 
+router.post("/", createUser);
 
-router.get("/", getAllUsers)
-
-router.post("/", createUser)
-
-router.get("/:id", getUser)
-
+router.get("/:id", getUser);
 
 module.exports = router;
--- Day07/server.js
+++ Day07/server.js
@@ -1,13 +1,11 @@
-const express = require('express');
-const bodyParser = require('body-parser')
-const usersRoutes = require('./routes/userRoutes')
+const express = require("express");
+const bodyParser = require("body-parser");
+const usersRoutes = require("./routes/userRoutes");
 const app = express();
 
 app.use(bodyParser.json());
 const port = 5000;
 
+app.use("/api/v1/users", usersRoutes);
 
-app.use("/api/v1/users", usersRoutes)
-
-
-app.listen(port, () => console.log(`My server is running on port ${port}`))
+app.listen(port, () => console.log(`My server is running on port ${port}`));
