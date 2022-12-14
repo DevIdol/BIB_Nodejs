d154ffb60e2355b41c39354d72d8f14a258fa68c
DevIdol <devidol.mm@gmail.com>
2022-12-14 10:36:14 +0630

    Add Get All Users

--- Day07/routes/userRoutes.js
+++ Day07/routes/userRoutes.js
@@ -1,8 +1,15 @@
 const express = require('express')
-
+const users = require('../user.json')
 const router = express.Router();
 
+const data = {
+  users,
+  setUser: function (data) {this.users = data}
+}
 
+router.get("/", (req, res) => {
+  res.send(data.users)
+})
 
 
 module.exports = router;
