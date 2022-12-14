1bf43b8c33b4ae0235ee0d308e6d1e888efd20a4
DevIdol <devidol.mm@gmail.com>
2022-12-14 10:35:54 +0630

    Add User Routes

--- Day07/server.js
+++ Day07/server.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser')
+const usersRoutes = require('./routes/userRoutes')
 const app = express();
 
-app.express(bodyParser.json());
+app.use(bodyParser.json());
 const port = 5000;
 
 
-
+app.use("/api/v1/users", usersRoutes)
 
 
 app.listen(port, () => console.log(`My server is running on port ${port}`))
