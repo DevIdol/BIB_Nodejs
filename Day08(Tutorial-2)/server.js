65d3eabe351dbc8873e48f281b37eb9629e445f6
DevIdol <devidol.mm@gmail.com>
2022-12-15 11:15:00 +0630

    Add Middleware

--- Day08(Tutorial-2)/server.js
+++ Day08(Tutorial-2)/server.js
@@ -1,10 +1,11 @@
 const express = require('express')
+const calcRoutes = require("./routes/calcRoutes")
 const app = express();
 const PORT = 5000;
 
 
+app.use("/api", calcRoutes)
 
 
 
-
-app.listen(PORT, () => console.log(`My server is connected on https://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`My server is connected on http://localhost:${PORT}`));
