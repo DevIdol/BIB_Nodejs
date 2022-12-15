3f2dd43cff69c9ed3500352e561dc83459f689a6
DevIdol <devidol.mm@gmail.com>
2022-12-15 13:22:22 +0630

    Add Middleware and Static File

--- Day08/server.js
+++ Day08/server.js
@@ -1,8 +1,13 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 
 const app = express();
 const PORT = 3000;
 
+//middleware
+app.use(bodyParser.json());
+
+//static file
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
