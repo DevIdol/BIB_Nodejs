e1b3ec3ca37d523c0ea176e841b51c17a08678c8
DevIdol <devidol.mm@gmail.com>
2022-12-15 12:28:45 +0630

    Code Refactoring

--- Day08(Tutorial-2)/routes/calcRoutes.js
+++ Day08(Tutorial-2)/routes/calcRoutes.js
@@ -1,21 +1,65 @@
-const express = require('express')
-const router = express.Router()
-
+const express = require("express");
+const router = express.Router();
 
 router.get("/sum/:num1/:num2", (req, res) => {
-  const sum = parseInt(req.params.num1) + parseInt(req.params.num2)
-  res.json({ Sum: `num1 + num2=> ${sum}` })
-})
+    const num1 = +req.params.num1;
+    const num2 = +req.params.num2;
+    res.json({ message: `Sum of ${num1} + ${num2}`, value: `${num1 + num2}` });
+});
 
 router.get("/subtract/:num1/:num2", (req, res) => {
-  const sub = parseInt(req.params.num1) - parseInt(req.params.num2)
-  console.log(sub)
-  res.json({ Subtract: `num1 - num2=> ${sub}` })
-})
+    const num1 = +req.params.num1;
+    const num2 = +req.params.num2;
+    res.json({
+        message: `Subtract of ${num1} - ${num2}`,
+        value: `${num1 - num2}`,
+    });
+});
 
 router.get("/multiply/:num1/:num2", (req, res) => {
-  const mult = parseInt(req.params.num1) / parseInt(req.params.num2)
-  res.json({ Multiply: `num1 / num2=> ${mult}` })
-})
+    const num1 = +req.params.num1;
+    const num2 = +req.params.num2;
+    res.json({
+        message: `Multipy of ${num1} * ${num2}`,
+        value: `${num1 * num2}`,
+    });
+});
+
+router.get("/divide/:num1/:num2", (req, res) => {
+    const num1 = +req.params.num1;
+    const num2 = +req.params.num2;
+    res.json({
+        message: `Divide of ${num1} / ${num2}`,
+        value: `${num1 / num2}`
+    });
+});
+
+router.get("/modulo/:num1/:num2", (req, res) => {
+    const num1 = +req.params.num1;
+    const num2 = +req.params.num2;
+    res.json({
+        message: `Modulo of ${num1} % ${num2}`,
+        value: `${num1 % num2}`
+    });
+});
+
+router.get("/area-of-cicle/:radius", (req, res) => {
+    const PI = Math.PI;
+    const radius = +req.params.radius;
+    res.json({
+        message: `Area of Cicle (Radius: ${radius})`,
+        value: `${PI * radius ** 2}`
+    });
+});
+
+router.get("/feet-inch-to-cm/:feet/:inch", (req, res) => {
+    const feet = +req.params.feet;
+    const inch = +req.params.inch;
+    const cm = feet * 30.48 + inch * 2.54;
+    res.json({
+        message: `${feet}feet + ${inch}inches to CM Conversion`,
+        value: `${cm}cm`,
+    });
+});
 
 module.exports = router;
--- Day08(Tutorial-2)/server.js
+++ Day08(Tutorial-2)/server.js
@@ -3,9 +3,6 @@ const calcRoutes = require("./routes/calcRoutes")
 const app = express();
 const PORT = 5000;
 
-
 app.use("/api", calcRoutes)
-
-
 
 app.listen(PORT, () => console.log(`My server is connected on http://localhost:${PORT}`));
