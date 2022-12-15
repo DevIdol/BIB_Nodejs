5ac6b35bc3028cdafd4c90fe8342b3244f012d7c
DevIdol <devidol.mm@gmail.com>
2022-12-15 11:15:53 +0630

    Add Subtract Two Params

--- Day08(Tutorial-2)/routes/calcRoutes.js
+++ Day08(Tutorial-2)/routes/calcRoutes.js
@@ -7,6 +7,12 @@ router.get("/sum/:num1/:num2", (req, res) => {
   res.json({ Sum: `num1 + num2=> ${sum}` })
 })
 
+router.get("/subtract/:num1/:num2", (req, res) => {
+  const sub = parseInt(req.params.num1) - parseInt(req.params.num2)
+  console.log(sub)
+  res.json({ Subtract: `num1 - num2=> ${sub}` })
+})
+
 
 
 module.exports = router;
