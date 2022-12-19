b0ec5ab0eb534f117a5c0cb2f6bf65723365d24f
DevIdol <devidol.mm@gmail.com>
2022-12-19 12:08:27 +0630

    Modified path

--- Day10(Tutorial-3)/server.js
+++ Day10(Tutorial-3)/server.js
@@ -2,7 +2,7 @@ import express from "express"
 import dotenv from "dotenv"
 import mongoose from "mongoose";
 import bodyParser from "body-parser"
-import bookRouter from "./routes/book.route";
+import bookRouter from "./routes/book.route.js";
 
 
 dotenv.config();
