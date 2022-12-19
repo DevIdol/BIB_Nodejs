97a81e0e4ca2df6ed9bc40015530107834cd6a11
DevIdol <devidol.mm@gmail.com>
2022-12-19 11:37:31 +0630

    Create Book Model

---
+++ Day10(Tutorial-3)/models/book.model.js
@@ -0,0 +1,25 @@
+import { Schema, model } from "mongoose";
+
+
+const bookSchema = new Schema({
+    name: {
+        type: String,
+        trim: true,
+        required: true,
+    },
+    author: {
+        type: String,
+        trim: true,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    year: {
+        type: Number,
+        required: true
+    }
+})
+
+export default model("books", bookSchema)
