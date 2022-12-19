bdf4d47c6a20b42e8223b2a407b9abc04ace1b3c
DevIdol <devidol.mm@gmail.com>
2022-12-19 12:07:23 +0630

    Add Route

--- Day10(Tutorial-3)/server.js
+++ Day10(Tutorial-3)/server.js
@@ -2,6 +2,7 @@ import express from "express"
 import dotenv from "dotenv"
 import mongoose from "mongoose";
 import bodyParser from "body-parser"
+import bookRouter from "./routes/book.route";
 
 
 dotenv.config();
@@ -18,4 +19,6 @@ mongoose.set("strictQuery", false);
 mongoose.connect(process.env.MONGO_DB || "").then(() => {
     console.log(`MongoDB Connected!`)
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+    //route
+    app.use('/api/books', bookRouter)
 }).catch(() => console.log(`MongoDB couldn't connect!`))
