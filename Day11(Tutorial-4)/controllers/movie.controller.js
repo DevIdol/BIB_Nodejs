e3de4ea197406813311ad17c5fb1cb9adc679a22
DevIdol <devidol.mm@gmail.com>
2022-12-20 11:22:52 +0630

    Update Route

--- Day11(Tutorial-4)/controllers/movie.controller.js
+++ Day11(Tutorial-4)/controllers/movie.controller.js
@@ -1,20 +1,60 @@
 import Movie from "../models/movie.model.js"
 
 
+//create movie
+export const createMovie = async (req, res) => {
+    const newMovie = new Movie(req.body)
+    try {
+        const savedMovie = await newMovie.save();
+        res.status(201).json({
+            message: "Create Success",
+            data: savedMovie,
+        })
+    } catch (error) {
+        res.status(500).json({
+            message: "Internal Server Error!",
+            error: error
+        })
+    }
+}
 
 
 //get movies
 export const getMovies = async (req, res) => {
-  try {
-    const movies = await Movie.find();
-    res.status(200).json({
-      message: "Success",
-      data: movies
-    })
-  } catch (error) {
-    res.status(500).json({
-      message: "Internal Server Error!",
-      error: error
-    })
-  }
+    try {
+        const movies = await Movie.find();
+        res.status(200).json({
+            message: "Success",
+            data: movies
+        })
+    } catch (error) {
+        res.status(500).json({
+            message: "Internal Server Error!",
+            error: error
+        })
+    }
+}
+
+
+//update movie 
+export const updateMovie = async (req, res) => {
+    const { id } = req.params.id;
+    const { name, rating, actor, director } = req.body
+    try {
+        const movie = await Movie.find(m => m.id == id)
+        if (name) movie.name == name
+        if (rating) movie.rating == rating
+        if (actor) movie.actor == actor
+        if (director) movie.director = director
+
+        res.status(200).json({
+            message: "Updated Success!",
+            data: movie
+        })
+    } catch (error) {
+        res.status(500).json({
+            message: "Internal Server Error!",
+            error: error
+        })
+    }
 }
--- Day11(Tutorial-4)/routes/movie.route.js
+++ Day11(Tutorial-4)/routes/movie.route.js
@@ -1,8 +1,13 @@
 import express from "express"
-import { getMovies } from "../controllers/movie.controller.js";
+import { createMovie, getMovies, updateMovie } from "../controllers/movie.controller.js";
 
 const router = express.Router();
 
+//create movie
+router.post('/', createMovie)
+
+//update movie
+router.post('/:id', updateMovie)
 
 //get movies
 router.get('/', getMovies)
