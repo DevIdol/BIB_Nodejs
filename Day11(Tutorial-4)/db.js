d3ad9801231ae5cbcc72183360a7cd86d0f8b205
DevIdol <devidol.mm@gmail.com>
2022-12-20 10:48:15 +0630

    Connect MongoDB

---
+++ Day11(Tutorial-4)/db.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+
+export const connectDB = async () => {
+    try {
+        await mongoose.set('strictQuery', true)
+        await mongoose.connect(process.env.MONGODB)
+        console.log('MongoDB connected!')
+    } catch (error) {
+        console.log("Couldn't connect to MongoDB", error)
+    }
+}
---
+++ Day11(Tutorial-4)/server.js
@@ -0,0 +1,19 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import dotenv from 'dotenv'
+import { connectDB } from './db.js'
+import mongoose from 'mongoose'
+
+dotenv.config()
+const app = express()
+const PORT = process.env.PORT || 8080
+
+//middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }))
+
+//db
+connectDB();
+
+
+app.listen(PORT, () => console.log(`Server running on port${PORT}`))
