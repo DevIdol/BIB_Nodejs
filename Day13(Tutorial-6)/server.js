63f2408a8cbaa8577d42c8e413f09a58cd0c0868
DevIdol <devidol.mm@gmail.com>
2022-12-26 11:12:04 +0630

    connect Routes

--- Day13(Tutorial-6)/server.js
+++ Day13(Tutorial-6)/server.js
@@ -2,19 +2,18 @@ import express, { application } from "express";
 import dotenv from 'dotenv'
 import bodyParser from "body-parser";
 import { connectDB } from "./db.js";
+import movieRoutes from './routes/movie.route.js'
 
 dotenv.config();
-
 const app = express();
 const PORT = process.env.PORT || 8080
 
 //middleware
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }))
 
 //db
 connectDB();
-
 
 //default route 
 app.get('/', (req, res) => {
@@ -23,5 +22,7 @@ app.get('/', (req, res) => {
     })
 })
 
+//routes api
+app.use('/api/movies', movieRoutes)
 
-app.listen(PORT, ()=> console.log(`server running on port ${PORT}`))
+app.listen(PORT, () => console.log(`server running on port ${PORT}`))
