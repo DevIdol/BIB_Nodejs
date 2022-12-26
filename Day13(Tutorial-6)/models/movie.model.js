3d6245271e8f3b69b4656445112e4ecea95a1e48
DevIdol <devidol.mm@gmail.com>
2022-12-26 10:34:10 +0630

    create movie schema

---
+++ Day13(Tutorial-6)/models/movie.model.js
@@ -0,0 +1,22 @@
+import { Schema, model } from "mongoose";
+
+const MovieSchema = new Schema({
+    name: {
+        type: String,
+        trim: true,
+        required: true,
+    },
+    year: {
+        type: String,
+        trim: true,
+        required: true,
+    },
+    rating: {
+        type: String,
+        trim: true,
+        requiredt: true
+    }
+
+}, { timestamps: true })
+
+export default model('movies', MovieSchema)
